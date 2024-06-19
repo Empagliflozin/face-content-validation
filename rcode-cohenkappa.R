fbddfd4a7ec6a598a6ffb64946fc6fa8d5e678d9
Bendgate <icquesri@gmail.com>
2024-06-19 14:43:13 +0700

    clarified comments

--- rcode-cohenkappa.R
+++ rcode-cohenkappa.R
@@ -1,8 +1,8 @@
 #define vector of packages to load
-# List of libraries to load
+# Define a vector of packages to load
 libraries <- c("ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", "forcats" , "vcd" , "psych" , "irr" , "krippendorfsalpha" , "ltm" )
 
-# Function to load each library
+# Function to install and load a package if it is not already installed
 lapply(libraries, function(lib) {
   if (!require(lib, character.only = TRUE)) {
     install.packages(lib, dependencies = TRUE)
