a7536d73b1c7133d162da583cf1a983cd693cf1a
Bendgate <icquesri@gmail.com>
2024-06-19 14:13:49 +0700

    changed filename

---
+++ rcode-cohenkappa.R
@@ -0,0 +1,12 @@
+#define vector of packages to load
+# List of libraries to load
+libraries <- c("ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", "forcats" , "vcd" , "psych" , "irr" , "krippendorfsalpha" )
+
+# Function to load each library
+lapply(libraries, function(lib) {
+  if (!require(lib, character.only = TRUE)) {
+    install.packages(lib, dependencies = TRUE)
+    library(lib, character.only = TRUE)
+  }
+})
+
