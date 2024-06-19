54b101c9e3d8a32815ebdd0a8373214705757c81
Bendgate <icquesri@gmail.com>
2024-06-19 19:23:15 +0700

    Major additions which include the cohen kappa calculation

--- rcode-cohenkappa.R
+++ rcode-cohenkappa.R
@@ -1,6 +1,7 @@
-#define vector of packages to load
 # Define a vector of packages to load
-libraries <- c("ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", "forcats" , "vcd" , "psych" , "irr" , "krippendorfsalpha" , "ltm" )
+libraries <- c("ggplot2", "dplyr", "tidyr", "readr", "purrr", "tibble", "stringr", 
+               "forcats" , "vcd" , "psych" , "irr" , "krippendorffsalpha" , "ltm" 
+               , "readxl" )
 
 # Function to install and load a package if it is not already installed
 lapply(libraries, function(lib) {
@@ -10,4 +11,18 @@ lapply(libraries, function(lib) {
   }
 })
 
+# reads the excel file containing the 'coded' questionnaire submission
+# Yes is coded as '1' and No is coded as '0'
+test1 <- read_excel("~/Desktop/test1.xlsx", col_names = TRUE ,
+                    range = "B1:C7")
+# prints out table of the excel sheet in a view tab
+view(test1)
+# checks column name
+colnames(test1)
+
+# converts the read excel file and reorganize it to a dataframe variable
+dftest1 <- data.frame(test1)
+
+# calculates cohen kappa
+cohen.kappa(x = dftest1)
 
