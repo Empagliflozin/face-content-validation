b5606d3afacacad1455129c20f9aa783a73598bc
Empagliflozin <icquesri@gmail.com>
2024-06-25 10:57:44 +0700

    changed some spacing and indentations

--- rcode-krippenalpha.R
+++ rcode-krippenalpha.R
@@ -1,7 +1,7 @@
 # Define a vector of packages to load
 libraries <- c("ggplot2", "dplyr", "tidyr" , "tibble", "stringr", 
-               "vcd" , "psych" , "irr" , "krippendorffsalpha" , "ltm" 
-               , "readxl")
+               "vcd", "psych", "irr", "krippendorffsalpha", "ltm", 
+               "readxl")
 
 # Function to install and load a package if it is not already installed
 lapply(libraries, function(lib) {
@@ -13,18 +13,24 @@ lapply(libraries, function(lib) {
 
 library(readxl)
 test1 <- read_excel("~/Desktop/test1.xlsx", 
-                    sheet = "Sheet2", range = "C4:L20")
+                    sheet = "Sheet2", 
+                    range = "C4:L20")
 View(test1)
 # 1 = yes 
 # 2 = no
 
-#converts test1 into a matrix 
+# converts test1 into a matrix 
 matrix1 <- as.matrix(test1)
+
+# views matrix created from test1
 view(matrix1)
 
 # employs customary method on nominal data
-finalalpha <- krippendorffs.alpha(matrix1 , level = "nominal" , method = "customary" , 
-                    confint = TRUE , control = list(bootit = 100000 , parallel = FALSE))
+finalalpha <- krippendorffs.alpha(matrix1 , 
+                                  level = "nominal" , 
+                                  method = "customary" , 
+                                  confint = TRUE , 
+                                  control = list(bootit = 100000 , parallel = FALSE))
 
 # prints out the summarized form of the alpha value and its 95% CI
 summary(finalalpha , conf.level = 0.95)
